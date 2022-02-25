4838895cffda902184b0a0790782ea61731a484c
Lord-Ordet <99984763+Lord-Ordet@users.noreply.github.com>
2022-02-25 10:24:20 +0100

    Delete data/mods/Quagsire Team Meta directory

--- data/mods/Quagsire Team Meta/megasitem.ts
+++
@@ -1,9 +0,0 @@
-infernapetite: {
-		name: "Infernapetite",
-		spritenum: 625,
-		megaStone: "Infernape-Mega",
-		megaEvolves: "Infernape",
-		itemUser: ["Infernape"],
-		onTakeItem(item, source) {
-			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
-			return true;
